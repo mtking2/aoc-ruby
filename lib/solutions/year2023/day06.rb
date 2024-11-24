b4f82c9f5534a3debd5a2c172dc4aea8f6513a62
Michael King <m.king@fastmail.com>
2024-11-24 12:42:05 -0500

    2023 day 6

--- lib/solutions/year2023/day06.rb
+++ lib/solutions/year2023/day06.rb
@@ -9,18 +9,44 @@ module Solutions
 			YEAR = 2023
 			DAY = 6
 
+			def get_wins(time, rec)
+				num_wins = 0
+				(0..time - 1).each do |i|
+					max_dist = i * (time - i)
+					# puts "#{i} - #{max_dist}"
+					num_wins += 1 if max_dist > rec
+				end
+				num_wins
+			end
+
 			def part1(input)
-				# Your code here
+				data = parse_input(input)
+				races = data.values.first.zip(data.values.last)
+				product = 1
+				races.each do |time, rec|
+					product *= get_wins(time, rec)
+				end
+				product
 			end
 
 			def part2(input)
-				# Your code here
+				data = parse_input(input)
+				time = data[:times].join.to_i
+				rec = data[:distances].join.to_i
+				# binding.b
+				get_wins(time, rec)
 			end
 
 			private
 
 			def parse_input(input)
-				# Process the input and return the result
+				times, distances = input.split("\n")
+				times.sub!(/Time:\s+/, "")
+				distances.sub!(/Distance:\s+/, "")
+				{
+					times: times.split(/\s+/).map(&:to_i),
+					distances: distances.split(/\s+/).map(&:to_i)
+				}
 			end
 
 		end
--- spec/solutions/year2023/day06_spec.rb
+++ spec/solutions/year2023/day06_spec.rb
@@ -20,13 +20,12 @@ RSpec.describe Solutions::Year2023::Day06 do
 		context "with example input" do
 			it "returns the expected result" do
 				example_input_part_one = AOCClient.fetch_example(described_class::YEAR, described_class::DAY, 1)
-				expect(solution.part1(example_input_part_one)).to eq("TODO")
+				expect(solution.part1(example_input_part_one)).to eq(288)
 			end
 		end
 
 		context "with actual input" do
 			it "returns the correct answer" do
-				skip("TODO")
 				part1_answer = AOCClient.fetch_answer(described_class::YEAR, described_class::DAY, 1)
 				expect(solution.part1(actual_input)).to eq(part1_answer)
 			end
@@ -36,15 +35,13 @@ RSpec.describe Solutions::Year2023::Day06 do
 	describe "#part2" do
 		context "with example input" do
 			it "returns the expected result" do
-				skip("TODO")
 				example_input_part_two = AOCClient.fetch_example(described_class::YEAR, described_class::DAY, 2)
-				expect(solution.part2(example_input_part_two)).to eq("TODO")
+				expect(solution.part2(example_input_part_two)).to eq(71503)
 			end
 		end
 
 		context "with actual input" do
 			it "returns the correct answer" do
-				skip("TODO")
 				part2_answer = AOCClient.fetch_answer(described_class::YEAR, described_class::DAY, 2)
 				expect(solution.part2(actual_input)).to eq(part2_answer)
 			end
