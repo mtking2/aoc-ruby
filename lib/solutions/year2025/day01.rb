d7cc18e05427bfe9041639ef72ef6538f0e909c3
Michael King <m.king@fastmail.com>
2025-12-04 11:10:25 -0500

    2025 day 1 part 2

--- lib/solutions/year2025/day01.rb
+++ lib/solutions/year2025/day01.rb
@@ -10,7 +10,6 @@ module Solutions
 			DAY = 1
 
 			def part1(input)
-				# Your code here
 				current_position = 50
 				num_zeros = 0
 				input.split("\n").each do |line|
@@ -18,14 +17,35 @@ module Solutions
 					direction = (direction == "R") ? 1 : -1
 					distance = distance.to_i * direction
 
-					new_position = (current_position += distance) % 100
+					new_position = (current_position + distance) % 100
 					num_zeros += 1 if new_position == 0
+					current_position = new_position
 				end
 				num_zeros
 			end
 
 			def part2(input)
-				# Your code here
+				current_position = 50
+				num_zeros = 0
+				input.split("\n").each do |line|
+					direction, distance = line.partition(/\d+/).reject(&:empty?)
+					direction = (direction == "R") ? 1 : -1
+					# distance = distance.to_i # * direction
+
+					goes_into = distance.to_i / 100
+					num_zeros += goes_into
+
+					remainder = (distance.to_i % 100) * direction
+					new_position = (current_position + remainder) % 100
+
+					# puts "#{line}, Goes Into: #{goes_into}, Current Position: #{current_position}, New Position: #{new_position}"
+					if new_position == 0 || (direction.positive? && new_position < current_position && current_position != 0) || (direction.negative? && new_position > current_position && current_position != 0)
+						num_zeros += 1
+						# puts "Reached zero"
+					end
+					current_position = new_position
+				end
+				num_zeros
 			end
 
 			private
--- spec/solutions/year2025/day01_spec.rb
+++ spec/solutions/year2025/day01_spec.rb
@@ -26,7 +26,6 @@ RSpec.describe Solutions::Year2025::Day01 do
 
 		context "with actual input" do
 			it "returns the correct answer" do
-				# skip("TODO")
 				part1_answer = AOCClient.fetch_answer(described_class::YEAR, described_class::DAY, 1)
 				expect(solution.part1(actual_input)).to eq(part1_answer)
 			end
@@ -36,15 +35,13 @@ RSpec.describe Solutions::Year2025::Day01 do
 	describe "#part2" do
 		context "with example input" do
 			it "returns the expected result" do
-				skip("TODO")
-				example_input_part_two = AOCClient.fetch_example(described_class::YEAR, described_class::DAY, 2)
-				expect(solution.part2(example_input_part_two)).to eq("TODO")
+				example_input_part_two = AOCClient.fetch_example(described_class::YEAR, described_class::DAY, 1)
+				expect(solution.part2(example_input_part_two)).to eq(6)
 			end
 		end
 
 		context "with actual input" do
 			it "returns the correct answer" do
-				skip("TODO")
 				part2_answer = AOCClient.fetch_answer(described_class::YEAR, described_class::DAY, 2)
 				expect(solution.part2(actual_input)).to eq(part2_answer)
 			end
